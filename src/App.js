4ed0e65050c37e455d8328f6c932f76f36f73b8b
Enknarin <s5935512076@phuket.psu.ac.th>
2019-09-22 17:22:36 +0700

    sheet final commait

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import CharacterCard from './CharacterCard.js';
+import WordCard from './WordCard.js';
+
+const word = "Hello";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WordCard value="Hello" />
+      </div>
+    );
+  }
+}
+export default App;
