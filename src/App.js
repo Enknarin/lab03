e34f89223da94de131c1f9558c250c9aa0f09e78
Enknarin <s5935512076@phuket.psu.ac.th>
2019-09-22 17:32:01 +0700

    commit final

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import CharacterCard from './CharacterCard.js';
 import WordCard from './WordCard.js';
 
-const word = "Hello";
+const word = "Doraemon";
 class App extends Component {
   render() {
     return (
-      <div>
-        <WordCard value="Hello" />
+      <div classname="App">
+        <header classname="App-header">
+          <h1 classname="App-title">Wellcome to React by Fiz</h1>
+        </header>
+        <WordCard value="Doraemon" />
       </div>
     );
   }
